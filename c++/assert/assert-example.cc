c25fc7d89abb9ef181b3f782bfefeffe05bdce21
cexposit <cexposit@ull.edu.es>
2022-02-13 23:37:06 +0000

    asserts

---
+++ c++/assert/assert-example.cc
@@ -0,0 +1,42 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Algoritmos y Estructuras de Datos Avanzadas
+ *
+ * @author Christopher Expósito Izquierdo
+ * @date 14 February 2022
+ * @brief assert macro example
+ * assert macro allows testing of boolean condition at run time
+ * Typically used to test sanity of code (e.g., test preconditions, postconditions, or other invariants) or test validity of assumptions made by code
+ * Defined in header file cassert
+ * Macro takes single argument: boolean expression
+ * If assertion fails, program is terminated by calling std::abort
+ * If NDEBUG preprocessor symbol is defined at time cassert header file included, all assertions are disabled (i.e., not checked)
+ *
+ * The assert macro checks for the validity of the assertions or assumptions. 
+ * If the assertion results to be FALSE then the macro writes information about the call that 
+ * failed on stderr and then calls abort(). 
+ * abort() raises the SIGABRT signal and this results in an abnormal termination of the process.
+ *
+ * @see https://en.cppreference.com/w/cpp/error/assert
+ */
+
+#include <iostream>
+#include <assert.h>
+
+// uncomment next line to disable assert()
+//#define NDEBUG 
+
+double MySqrt(double x) {
+  assert(x >= 0);
+  std::cout << "Execution in Mysqrt function" << std::endl;
+  return x;
+}
+
+int main() {
+  double param = -1.0;
+  const double result{MySqrt(param)};
+  std::cout << "param: " << param << " result: " << result << std::endl;
+  return 0;
+}
---
+++ c++/assert/assert-example2.cc
@@ -0,0 +1,36 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Algoritmos y Estructuras de Datos Avanzadas
+ *
+ * @author Christopher Expósito Izquierdo
+ * @date 14 February 2022
+ * @brief assert macro example
+ * assert macro allows testing of boolean condition at run time
+ * Typically used to test sanity of code (e.g., test preconditions, postconditions, or other invariants) or test validity of assumptions made by code
+ * Defined in header file cassert
+ * Macro takes single argument: boolean expression
+ * If assertion fails, program is terminated by calling std::abort
+ * If NDEBUG preprocessor symbol is defined at time cassert header file included, all assertions are disabled (i.e., not checked)
+ *
+ * @see https://en.cppreference.com/w/cpp/error/assert
+ */
+
+#include <iostream>
+#include <assert.h>
+
+// uncomment next line to disable assert()
+#define NDEBUG 
+ 
+// Use (void) to silent unused warnings.
+#define assertm(exp, msg) assert(((void)msg, exp))
+ 
+int main() {
+  assert(2 + 2 == 4);
+  std::cout << "Execution continues past the first assert" << std::endl;
+  assertm(2 + 2 == 5, "There are five lights");
+  std::cout << "Execution continues past the second assert" << std::endl;
+  assert((2 * 2 == 5) && "Yet another way to add assert message");
+  return 0;
+}
