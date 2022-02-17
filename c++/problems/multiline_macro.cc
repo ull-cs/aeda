72887ab03a1e00b66a067dfa18f3537f936a074d
cexposit <cexposit@ull.edu.es>
2022-02-17 11:39:09 +0000

    problem

---
+++ c++/problems/multiline_macro.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+  
+#define MACRO(num, str) { \
+            std::cout << num; \
+            std::cout << " is"; \
+            std::cout << " " << str; \
+            std::cout << "\n"; \
+           }
+  
+int main() {
+  int num;
+  std::cout << "Enter a number: ";
+  std::cin >> num;
+  if (num & 1) {
+    MACRO(num, "Odd");
+  } else {
+    MACRO(num, "Even");
+  }
+  return 0;
+}
