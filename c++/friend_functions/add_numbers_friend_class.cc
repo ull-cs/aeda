d0538edca9df1ae6c2f0ff35f941192ddd7efbe1
cexposit <cexposit@ull.edu.es>
2022-02-17 09:54:02 +0000

    friend_functions

---
+++ c++/friend_functions/add_numbers_friend_class.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+class ClassB;
+
+class ClassA {
+ public:
+  ClassA() : numA_(12) {}
+ private:
+  int numA_;
+  friend class ClassB;
+};
+
+class ClassB {
+ public:
+  ClassB() : numB_(1) {}
+  int AddNumbers(ClassA objectA) {
+    return (objectA.numA_ + numB_);
+  }
+ private:
+  int numB_;
+};
+
+int main() {
+  ClassA objectA;
+  ClassB objectB;
+  cout << "Sum: " << objectB.AddNumbers(objectA) << endl;
+  return 0;
+}
+
