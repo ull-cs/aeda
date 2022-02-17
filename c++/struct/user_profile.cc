59999602c191a0a9880979a4bee30123af4a6c3d
cexposit <cexposit@ull.edu.es>
2022-02-17 10:09:05 +0000

    structs

---
+++ c++/struct/user_profile.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+struct User {
+  std::string name;
+  std::string surname;
+  int age;
+};
+
+void PrintUser(const User& user) {
+  std::cout << "Name=" << user.name;
+  std::cout << ", surname=" << user.surname;
+  std::cout << ", age= " << user.age;
+  std::cout << std::endl;
+}
+
+int main() {
+  User user1{"Antonio", "Pérez", 27};
+  PrintUser(user1);
+  User user2;
+  user2.name = "Juana";
+  user2.surname = "Moreno";
+  user2.age = 31;
+  PrintUser(user2);
+  PrintUser({"Luisa", "Acosta", 24});
+  return 0;
+}
