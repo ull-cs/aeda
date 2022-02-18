33eaa68e62daeaa6457aa56faffa53af0f7e9bf9
cexposit <cexposit@ull.edu.es>
2022-02-18 23:47:52 +0000

    example

---
+++ c++/static/box_static_function_member.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+ 
+using namespace std;
+
+class Box {
+ public:
+  static int objectCount;
+  Box(double length = 2.0, double breadth = 2.0, double height = 2.0) {
+    cout <<"Constructor called." << endl;
+    length_ = length;
+    breadth_ = breadth;
+    height_ = height;
+    objectCount++;
+  }
+  double Volume() {
+    return length_ * breadth_ * height_;
+  }
+  static int getCount() {
+    return objectCount;
+  }
+ private:
+  double length_;
+  double breadth_;
+  double height_;
+};
+
+int Box::objectCount = 0;
+
+int main(void) {
+  cout << "Inital Stage Count: " << Box::getCount() << endl;
+  Box Box1(3.3, 1.2, 1.5);
+  Box Box2(8.5, 6.0, 2.0);
+  cout << "Final Stage Count: " << Box::getCount() << endl;
+  return 0;
+}
