4dd35f5f5898c4b8554d714e14eb8984ad7d7e98
cexposit <cexposit@ull.edu.es>
2022-02-23 16:33:41 +0000

    box.cc

---
+++ c++/templates/box.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+template<class T>
+class Box {
+ public:
+  Box(T data) {
+    data_ = data;
+  } 
+  void Print() { 
+    cout << data_ << endl;
+  }
+ private:
+  T data_;
+};
+
+template<>
+class Box<bool> {
+ public:
+  Box(bool data) {
+    data_ = data;
+  }
+  void Print() { 
+    cout << (data_ ? "true" : "false") << endl;
+  }
+ private:
+  bool data_;
+};
+
+int main () {
+  Box<int> intBox(27);
+  Box<bool> boolBox(true);
+  cout << "intBox: ";
+  intBox.Print();
+  cout << "boolBox: ";
+  boolBox.Print();
+  return 0;
+}
