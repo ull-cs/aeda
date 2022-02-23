ce0272956a2a9ef6045a40e52475ff2c33c1413d
cexposit <cexposit@ull.edu.es>
2022-02-23 16:44:47 +0000

    m

---
+++ c++/templates/max_multiple_parameters.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+ 
+template <typename T, typename U> // '>' must be overloaded
+auto Maximum(T x, U y) {
+  return (x > y)? x: y;
+}
+ 
+int main() {
+  cout << "Maximum between " << 3 << " and " << 7.5 << " is: " << Maximum(3, 7.5) << endl;
+  cout << "Maximum between " << 'p' << " and " << 7.0 << " is: " << Maximum('p', 7.0) << endl;
+  cout << "Maximum between " << 5 << " and " << 'e' << " is: " << Maximum(5, 'e') << endl;
+  return 0;
+}
