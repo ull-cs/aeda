bce9d866eb4eddca9a15931c928f057edb2d3f4e
cexposit <cexposit@ull.edu.es>
2022-03-01 12:04:47 +0000

    minor changes

---
+++ c++/operator_overloading/arithmetic_overloading/cents.h
@@ -0,0 +1,31 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author F. de Sande
+ * @date 10 Dec 2020
+ * @brief Declaration of the Cent class
+ */
+
+#ifndef CENTS_H
+#define CENTS_H
+
+#include <iostream>
+
+class Cents {
+ public:
+  Cents(int cents) : cents_{cents} {
+    std::cout << "Cents constructor has been called" << std::endl;
+  }
+
+  int cents() const { return cents_; }  // getter
+ private:
+  int cents_{};
+};
+
+// Need to explicitly provide prototype for operator+ so uses of operator+ in other files know this overload exists
+Cents operator+(const Cents& c1, const Cents& c2);
+
+#endif
