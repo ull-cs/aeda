d5c347b7dd6cf7311a545366e1194d9c372753fc
cexposit <cexposit@ull.edu.es>
2022-03-01 11:14:04 +0000

    operator overloading

---
+++ c++/operator_overloading/custom_operator.cc
@@ -0,0 +1,38 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Algoritmos y Estructuras de Datos Avanzadas
+ *
+ * @author Christopher Expósito Izquierdo
+ * @date 01 March 2022
+ * @brief Custom operators
+ *        Operators are functions with a signature:
+ *          <RETURN_TYPE> operator<NAME>(<PARAMS>)
+ *        <NAME> represents the target operation, e.g. >, <, =, ==, << etc.
+ *        Have all attributes of functions
+ *        Always contain word operator in name
+ *
+ * @see https://en.cppreference.com/w/cpp/language/operators
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+class Human {
+ public:
+  Human(int kindness) : kindness_{kindness} {}
+  bool operator<(const Human& other) const {
+    return kindness_ < other.kindness_;
+  }
+ private:
+  int kindness_ = 100;
+};
+
+int main () {
+  std::vector<Human> humans = {Human{0}, Human{10}};
+  std::sort(humans.begin(), humans.end());
+  return 0;
+}
+
