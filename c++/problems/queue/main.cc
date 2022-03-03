0b0311f407de4351ca00872ad52af27aa81a833e
cexposit <cexposit@ull.edu.es>
2022-03-03 10:15:04 +0000

    queue

---
+++ c++/problems/queue/main.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stdlib.h>
+#include "queue.h"
+
+using namespace std;
+
+const std::string kHelpText = "Este programa toma como parámetro un número y crea una cola con dicho número de elementos para luego vaciarla";
+
+void Usage(int argc, char *argv[]) {
+  if (argc == 2) {
+    std::string parameter{argv[1]};
+    if (parameter == "--help") {
+      std::cout << kHelpText << std::endl;
+      exit(EXIT_FAILURE);
+    }
+  } 
+  else {
+    std::cout << "El programa requiere un parámetro." << std::endl;
+    std::cout << "Modo de uso:" << std::endl;
+    std::cout << argv[0] << " NUMERO" << std::endl;
+    std::cout << "Pruebe " << argv[0] << " --help para más información" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  Usage(argc, argv);
+  const int kElements = stoi(argv[1]);
+  srand(time(NULL));
+  Queue<int> queue;
+  for (int i = 0; i < kElements; i++) {
+    const int randomNumber = rand() % 10 + 1;
+    queue.push(randomNumber);
+    cout << randomNumber << " has been pushed" << endl;
+  }
+  cout << "Elements present in queue: ";
+  while (!queue.isEmpty()) {
+    cout << queue.peek() << " ";
+    queue.pop();
+  }
+  cout << endl;
+  return 0;
+}
---
+++ c++/problems/queue/queue.h
@@ -0,0 +1,79 @@
+#include "node.h"
+
+#ifndef QUEUE
+#define QUEUE
+
+using namespace std;
+
+template <class T>
+class Queue
+{
+public:
+    Queue();
+    bool isEmpty();
+    void push(T v);
+    T peek();
+    void pop();
+
+private:
+    Node<T> *start;
+    Node<T> *end;
+};
+
+template <typename T>
+Queue<T>::Queue()
+{
+    start = end = NULL;
+}
+
+template <typename T>
+bool Queue<T>::isEmpty()
+{
+    return start == NULL;
+}
+
+template <typename T>
+void Queue<T>::push(T v)
+{
+    Node<T> *temp = new Node<T>(v);
+    if (isEmpty())
+    {
+        start = end = temp;
+    }
+    else
+    {
+        end->next = temp;
+        end = temp;
+    }
+}
+
+template <typename T>
+T Queue<T>::peek()
+{
+    if (isEmpty())
+        return -99999;
+    else
+        return start->data;
+}
+
+template <typename T>
+void Queue<T>::pop()
+{
+    if (isEmpty())
+    {
+        cout << "Queue is Empty" << endl;
+    }
+    else if (start == end)
+    {
+        delete start;
+        start = end = NULL;
+    }
+    else
+    {
+        Node<T> *temp = start;
+        start = start->next;
+        delete temp;
+    }
+}
+
+#endif
