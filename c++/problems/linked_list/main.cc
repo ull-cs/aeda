63d15ecb99e12b73ab920619ea2dd7d299e695ed
cexposit <cexposit@ull.edu.es>
2022-03-03 09:56:40 +0000

    linked list

---
+++ c++/problems/linked_list/main.cc
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <stdlib.h>
+#include "linkedlist.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  LinkedList<int> list;
+  int option;
+  int item;
+  int index;
+  bool quit = false;
+  do
+  {
+    cout << "====================================" << endl;
+    cout << "Select option :" << endl;
+    cout << "1: Insert back" << endl;
+    cout << "2: Insert front" << endl;
+    cout << "3: Insert at index" << endl;
+    cout << "4: Display items" << endl;
+    cout << "5: Get item at index" << endl;
+    cout << "6: Delete back" << endl;
+    cout << "7: Delete front" << endl;
+    cout << "8: Delete at index" << endl;
+    cout << "9: Exit" << endl;
+    cout << "Option: ";
+    cin >> option;
+    cout << "====================================" << endl;
+    switch (option)
+    {
+    case 1:
+      cout << "Enter item to insert: ";
+      cin >> item;
+      list.add(item);
+      break;
+    case 2:
+      cout << "Enter item to insert: ";
+      cin >> item;
+      list.addFront(item);
+      break;
+    case 3:
+      cout << "Enter item to insert: ";
+      cin >> item;
+      cout << "Enter index: ";
+      cin >> index;
+      list.add(index, item);
+      break;
+    case 4:
+      list.displayAll();
+      break;
+    case 5:
+      cout << "Enter index: ";
+      cin >> index;
+      cout << "Item at index " << index << ": " << list.get(index) << endl;
+      break;
+    case 6:
+      list.remove();
+      break;
+    case 7:
+      list.removeFront();
+      break;
+    case 8:
+      cout << "Enter index: ";
+      cin >> index;
+      list.remove(index);
+      break;
+    case 9:
+      quit = true;
+      break;
+    default:
+      cout << "Invalid selection" << endl;
+      break;
+    }
+  } while (!quit);
+  return 0;
+}
---
+++ c++/problems/linked_list/node.h
@@ -0,0 +1,16 @@
+#ifndef NODE
+#define NODE
+
+template<typename T>
+class Node {
+ public:
+  Node() {
+    this->next = NULL;
+  }
+ private:
+  T data;
+  Node<T>* next;
+  template<typename U>friend class LinkedList;  
+};
+
+#endif
