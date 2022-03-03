a517ad56a727f2447e53467bebe731c8ac8696e0
cexposit <cexposit@ull.edu.es>
2022-03-03 11:18:15 +0000

    minor changes

---
+++ c++/operator_overloading/point_function_call.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class Point {
+ public:
+  Point() : x(0), y(0) { }
+  Point& operator()(int dx, int dy) {
+    x += dx;
+    y += dy;
+    return *this;
+  }
+ private:
+  int x;
+  int y;
+};
+
+int main() {
+  Point pt;
+  // Offset this coordinate x with 3 points and coordinate y with 2 points.
+  pt(3, 2);
+  return 0;
+}
