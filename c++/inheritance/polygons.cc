918b3c0b6be42003394a4e53459ea7360c96a5b3
cexposit <cexposit@ull.edu.es>
2022-03-07 08:55:17 +0000

    minor changes

---
+++ c++/inheritance/polygons.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+class Polygon {
+ public:
+  void setValues(int width, int height) {
+    width_ = width;
+    height_ = height;
+  }
+
+ protected:
+  int width_;
+  int height_;
+};
+
+class Rectangle : public Polygon {
+ public:
+  int area() {
+    return width_ * height_;
+  }
+};
+
+class Triangle : public Polygon {
+ public:
+  int area() {
+    return width_ * height_ / 2;
+  }
+};
+
+int main() {
+  Rectangle rectangle;
+  Triangle triangle;
+  rectangle.setValues(4, 5);
+  triangle.setValues(4, 5);
+  cout << "Area: " << rectangle.area() << '\n';
+  cout << "Area: " << triangle.area() << '\n';
+  return 0;
+}
