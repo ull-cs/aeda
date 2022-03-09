02d988c41a536dc6af3902c7be70b568dc950b4f
cexposit <cexposit@ull.edu.es>
2022-03-09 16:56:53 +0000

    new codes

---
+++ c++/inheritance/final_class.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+class Unique final {
+
+ public:
+  void display() {
+   cout << "My unique class" << endl;
+  }
+};
+
+// class Derived : Unique {
+// YOUR GET ERROR HERE if you inherit the Unique class
+// class Unique - a final class cannot be used as
+// a base class.
+//};
+
+int main() {
+  Unique object;
+  object.display(); 
+  return 0;
+}
+
