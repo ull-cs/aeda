bd1abfad9bdef50d9f7ed73695306452624763df
cexposit <cexposit@ull.edu.es>
2022-03-09 17:02:48 +0000

    new codes

---
+++ c++/inheritance/final_method.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+class Unique{
+ public:
+	static Unique*  GetInstance(){
+		return new Unique();
+	}	
+	void Display() {
+		cout<<" My Unique Class"<<endl; 
+        }
+ private:
+	Unique() { }
+}; 
+
+int main(){ 
+  Unique *u = Unique::GetInstance(); 
+  u->Display();		
+  return 0;
+}
