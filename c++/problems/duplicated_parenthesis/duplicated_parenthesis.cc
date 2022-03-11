f14155d2c5acb4d88dbac3f236a3314cfc68a4fc
cexposit <cexposit@ull.edu.es>
2022-03-11 15:54:04 +0000

    new codes

---
+++ c++/problems/duplicated_parenthesis/duplicated_parenthesis.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool hasDuplicateParenthesis(string expression) {
+  if (expression.length() <= 3) {
+    return false;
+  }
+  // take an empty stack of characters
+  stack<char> stack;
+  // traverse the input expression
+  for (char character : expression) {
+    // if the current char in the expression is not a closing parenthesis
+    if (character != ')') {
+      stack.push(character);
+    }
+    // if the current char in the expression is a closing parenthesis
+    else {
+      // if the stack's top element is an opening parenthesis,
+      // the subexpression of the form ((exp)) is found
+      if (stack.top() == '(') {
+        return true;
+      }
+      // pop till '(' is found for current ')'
+      while (stack.top() != '(') {
+        stack.pop();
+      }
+      // pop '('
+      stack.pop();
+    }
+  }
+  // if we reach here, then the expression does not have any duplicate parenthesis
+  return false;
+}
+
+int main(int argc, char *argv[]) {
+  const std::string expression = argv[1];
+  if (hasDuplicateParenthesis(expression)) {
+    cout << "Expression '" << expression << "' has duplicated parenthesis." << endl;
+  } else {
+    cout << "Expression '" << expression << "' does not have duplicated parenthesis." << endl;
+  }
+  return 0;
+}
